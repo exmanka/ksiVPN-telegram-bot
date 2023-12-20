55cc3c78c4ce597f3b0c6284432fd4bfe0f66e92
exmanka <exmanka@gmail.com>
2023-12-20 21:48:33 +0300

    feat(main): clear repo

--- src/services/script_generate_ref_promocodes.py
+++
@@ -1,8 +0,0 @@
-import string
-import random
-
-
-max_id = 43
-promocode_random_part_length = 7
-for i in range(max_id):
-    print(f"INSERT INTO promocodes_ref(phrase, ref_client_id) VALUES(\'{'REF' + ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(promocode_random_part_length))}\', {i + 1});")
