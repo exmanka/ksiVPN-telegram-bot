def2b8733c47a90fc71d8ff436aec82dabf2d0b6
exmanka <exmanka@gmail.com>
2024-01-15 14:09:24 +0300

    feat(deployment): add console logging

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ def main() -> None:
 
 if __name__ == '__main__':
     # add logging
-    logging.basicConfig(filename='bot.log',
+    logging.basicConfig(handlers=[logging.FileHandler('bot.log'), logging.StreamHandler()],
                         level=logging.INFO,
                         format='[%(asctime)s: %(levelname)s: %(name)s] %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S',
