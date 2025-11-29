5e9244fb9f99c5aa5e6b8d17e33ad62ca3851b70
exmanka <exmanka@gmail.com>
2025-11-29 18:09:52 +0300

    build(database): update Dockerfile

--- build/database/init-database-backup-cron.sh
+++ build/database/init-database-backup-cron.sh
@@ -1,10 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+# Set strict mode for script
+set -euo pipefail
 
-echo "Running script for creating scheduler for daily database backups:"
-echo "- starting cron service"
+# Set colors for logging
+LOG_COLOR="\e[95m"
+LOG_COLOR_CLEAR="\e[0m"
+
+# Start cron service
+echo -e "${LOG_COLOR}[init-database-backup-cron.sh] Running script for creating scheduler for daily database backups${LOG_COLOR_CLEAR}"
+echo -e "${LOG_COLOR}[init-database-backup-cron.sh] Starting cron service${LOG_COLOR_CLEAR}"
 service cron start
-echo "- adding cron job to schedule database backups"
+
+# Add cron job for daily database backups at 19:55
+echo -e "${LOG_COLOR}[init-database-backup-cron.sh] Adding cron job to schedule database backups${LOG_COLOR_CLEAR}"
 (echo "55 19 * * * pg_dump -U $POSTGRES_USER --no-password --clean --if-exists --format=plain $POSTGRES_DB | gzip > /var/lib/postgresql/backups/db-backup.gz") | crontab -
-echo "- executing something important for postgres container integrity"
-exec "$@"
+echo -e "${LOG_COLOR}[init-database-backup-cron.sh] Executing Postgres entrypoint${LOG_COLOR_CLEAR}"
+exec docker-entrypoint.sh "$@"
